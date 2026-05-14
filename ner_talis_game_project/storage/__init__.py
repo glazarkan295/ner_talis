6fc1ac37e252166400145b46010f489c445d014b
glazarkan295 <glazarkan295@gmail.com>
2026-05-14 13:11:27 +0300

    Patch storage classes with starter pack preservation

--- ner_talis_game_project/storage/__init__.py
+++ ner_talis_game_project/storage/__init__.py
@@ -1,7 +1,9 @@
 from storage.base import PlayerStorage
 from storage.hard_delete_runtime import patch_known_storage_classes
+from storage.starter_pack_runtime import patch_known_starter_pack_storage_classes
 from storage.storage_factory import create_storage
 
 patch_known_storage_classes()
+patch_known_starter_pack_storage_classes()
 
 __all__ = ["PlayerStorage", "create_storage"]
